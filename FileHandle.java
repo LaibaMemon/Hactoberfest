ba4fbd714c6191c0fa5f37e59a7e888150bdd5d0
LaibaMemon <49434426+LaibaMemon@users.noreply.github.com>
2020-10-18 00:09:33 +0500

    Update FileHandle.java

--- FileHandle.java
+++ FileHandle.java
@@ -8,3 +8,14 @@ class FileHandle{
  }
 
 }
+
+import java.io.*;
+class FileHandle{
+ public static void main(String[] args){
+   File file = new File("Location");
+   if(file.exists()){
+     System.out.println("File not  Found");
+   }
+ }
+
+}
